d73728e1d1b3e0f1a6e2428e829fe63f2333b69b
bringha <bringha@viena>
2015-06-08 20:35:13 -0300

    Criada classe Time

---
+++ src/java/br/edu/ifsul/modelo/Time.java
@@ -0,0 +1,133 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifsul.modelo;
+
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+
+/**
+ *
+ * @author bringha
+ */
+@Entity
+@Table(name = "time")
+public class Time implements Serializable{
+    @Id
+    @SequenceGenerator(name = "seq_id_time", sequenceName = "seq_time_id", allocationSize = 1)
+    @GeneratedValue(generator = "seq_id_time", strategy = GenerationType.SEQUENCE)
+    private Integer id;
+    @Length(max = 100, message = "O nome não pode ultrapassar {max} caracteres")
+    @NotEmpty(message = "O nome deve ser informado")
+    @Column(name = "nome", length = 100, nullable = false)
+    private String nome;
+    @Column(name = "vitorias")
+    private Integer vitorias;
+    @Column(name = "empates")
+    private Integer empates;
+    @Column(name = "derrotas")
+    private Integer derrotas;
+    @Column(name = "golsPro")
+    private Integer golsPro;
+    @Column(name = "golsContra")
+    private Integer golsContra;
+
+    public Time() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getVitorias() {
+        return vitorias;
+    }
+
+    public void setVitorias(Integer vitorias) {
+        this.vitorias = vitorias;
+    }
+
+    public Integer getEmpates() {
+        return empates;
+    }
+
+    public void setEmpates(Integer empates) {
+        this.empates = empates;
+    }
+
+    public Integer getDerrotas() {
+        return derrotas;
+    }
+
+    public void setDerrotas(Integer derrotas) {
+        this.derrotas = derrotas;
+    }
+
+    public Integer getGolsPro() {
+        return golsPro;
+    }
+
+    public void setGolsPro(Integer golsPro) {
+        this.golsPro = golsPro;
+    }
+
+    public Integer getGolsContra() {
+        return golsContra;
+    }
+
+    public void setGolsContra(Integer golsContra) {
+        this.golsContra = golsContra;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 59 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Time other = (Time) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Time{" + nome + '}';
+    }
+
+}
